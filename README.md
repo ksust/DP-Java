e07ba5976c9428984602019ff2728d82f6f211ee
ksust <admin@ksust.com>
2018-01-22 21:27:42 +0800

    DP-Java 学习
    Signed-off-by: ksust <admin@ksust.com>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DP-Java study
 Java 设计模式：Design Pattern for Java
+从头学DP
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Java 设计模式学习：DP-Java
+ * 外部 主函数
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java 设计模式：Design Pattern for Java");
+    }
+}
